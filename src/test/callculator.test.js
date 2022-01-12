a0ed23966e9ea3bea7f9e1716d2cc1f02c4cc2e0
hanamDeveloper <zmf1118@gmail.com>
2022-01-12 13:30:27 +0900

    Add Calculator test

---
+++ src/test/callculator.test.js
@@ -0,0 +1,41 @@
+const { test } = require('@jest/globals');
+const Calculator = require('../calculator');
+
+describe('Calculator Test', () => {
+    const Calculator1 = new Calculator;
+    test('default value', () => {
+        expect(Calculator1.value).toBe(0)
+    })
+
+    test('add 1', () => {
+        const num = 1;
+        Calculator1.add(num);
+        expect(Calculator1.value).toBe(num)
+    })
+
+    test('clear', () => {
+        Calculator1.clear();
+        expect(Calculator1.value).toBe(0)
+    })
+
+    test('subtract', () => {
+        const num = 2;
+        Calculator1.subtract(num);
+        expect(Calculator1.value).toBe(-2)
+    })
+
+    test('multiply', () => {
+        Calculator1.clear();
+        const num = 2;
+        Calculator1.multiply(num);
+        expect(Calculator1.value).toBe(Calculator1.value * num)
+    })
+
+    test('divide', () => {
+        Calculator1.clear();
+        const num = 2;
+        Calculator1.divide(num);
+        expect(Calculator1.value).toBe(Calculator1.value / num)
+    })
+})
+
