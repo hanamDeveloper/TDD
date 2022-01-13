0cc37b16b5232ffafffaaede27e1b264ee8cb281
“yskim“ <yskim@builton.co.kr>
2022-01-13 15:10:34 +0900

    Add Stack Test

---
+++ src/challange/stack.test.js
@@ -0,0 +1,12 @@
+
+
+
+describe('test Stack', () => {
+    const array1 = [];
+    const result = [1];
+
+    it('sholud be Index', () => {
+        array1.push(1)
+        expect(array1).toEqual(result)
+    })
+})
